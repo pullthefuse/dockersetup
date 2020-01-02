e13d1c17f2f58751b142d7222824eb1ebe86339b
James Castro <james@castro.com>
2020-01-02 14:09:43 +0000

    Fix for setting up site without ssl

--- tests/TestFiles/nginxProxyServerBlock.php
+++ tests/TestFiles/nginxProxyServerBlock.php
@@ -1,11 +1,11 @@
 server {
-    listen 80 http2;
-    listen [::]:80 http2;
+    listen 80;
+    listen [::]:80;
 
     server_name dev.example.com;
 
     resolver 127.0.0.11 valid=5s;
-    set $upstream https://nginx_dev_example_com;
+    set $upstream http://nginx_dev_example_com;
 
     location / {
         proxy_set_header Host $host;
